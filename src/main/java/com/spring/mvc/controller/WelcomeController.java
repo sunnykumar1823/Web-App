4113665525c02a447434b737cc6da07a60b44912
sunnykumar2001 <sunnykumar18122001@gmail.com>
2026-03-09 20:50:46 +0530

    update

--- src/main/java/com/spring/mvc/controller/WelcomeController.java
+++ src/main/java/com/spring/mvc/controller/WelcomeController.java
@@ -3,6 +3,7 @@ package com.spring.mvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -22,11 +23,21 @@ public class WelcomeController {
 	}
 
 	@GetMapping("/req-param")
-	public String createUser(@RequestParam(name = "user") String name, Model model) {
+	public String createUser(@RequestParam(name = "user", defaultValue = "MY-DEFAULT-NAME") String name, Model model) {
 
 		model.addAttribute("user", name);
 
 		System.out.println("WelcomeController.greeting :" + name);
+
+		return "success2";
+	}
+
+	@GetMapping("/path-var/{id}")
+	public String pathVarExample(@PathVariable(name = "id") int id, Model model) {
+
+		model.addAttribute("user", "Sorry this : " + id + " User is not found in our db");
+
+		System.out.println("WelcomeController.Path-var :" + id);
 
 		return "success2";
 	}
